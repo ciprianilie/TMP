4fdf67bc8ad044a443e9df993e81b37e6fcdb470
Marina <marina.zabica@endava.com>
2023-07-25 13:32:35 +0300

    Workshop1: Updated db context

--- TMS.Api/TMS.Api/Program.cs
+++ TMS.Api/TMS.Api/Program.cs
@@ -10,12 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-var connectionString = builder.Configuration.GetConnectionString("TMSConnectionString");
+
 builder.Services.AddTransient<IEventRepository, EventRepository>();
-
-
-
-builder.Services.AddDbContext<TmsTestContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
